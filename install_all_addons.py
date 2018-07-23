d8ae3ab5f6baf0ee965548f8df37e1a4b331a8aa
CKrawczyk <coleman.krawczyk@gmail.com>
2018-07-23 11:04:18 +0100

    Update install script with full file paths
    This is needed to make the script run on Windows.  The `os` package is 
    used to make sure it will run under any OS.

--- install_all_addons.py
+++ install_all_addons.py
@@ -1,11 +1,17 @@
 import bpy
+import os
+
+# get current directory
+current_dir = os.getcwd()
 
 # install and activate `emboss plane`
-bpy.ops.wm.addon_install(filepath='emboss_plane.py')
+emboss_plane_filepath = os.path.join(current_dir, 'emboss_plane.py')
+bpy.ops.wm.addon_install(filepath=emboss_plane_filepath)
 bpy.ops.wm.addon_enable(module='emboss_plane')
 
 # install and activate `name plate`
-bpy.ops.wm.addon_install(filepath='name_plate.py')
+name_plate_filepath = os.path.join(current_dir, 'name_plate.py')
+bpy.ops.wm.addon_install(filepath=name_plate_filepath)
 bpy.ops.wm.addon_enable(module='name_plate')
 
 # save user preferences
