041af2602b4648f3f8cb49ea3ce95c1d96a68f75
CKrawczyk <coleman.krawczyk@gmail.com>
2020-02-24 09:27:01 +0000

    Update install addon script
    Multi-file addons must be in zipped in order to be installed.

--- install_all_addons.py
+++ install_all_addons.py
@@ -5,22 +5,12 @@ import os
 current_dir = os.getcwd()
 
 # install and activate `emboss plane`
-emboss_plane_filepath = os.path.join(current_dir, 'emboss_plane.py')
-bpy.ops.wm.addon_install(filepath=emboss_plane_filepath)
-bpy.ops.wm.addon_enable(module='emboss_plane')
-
-# install and activate `name plate`
-name_plate_filepath = os.path.join(current_dir, 'name_plate.py')
-bpy.ops.wm.addon_install(filepath=name_plate_filepath)
-bpy.ops.wm.addon_enable(module='name_plate')
-
-# install and activate `holder`
-holder_filepath = os.path.join(current_dir, 'holder.py')
-bpy.ops.wm.addon_install(filepath=name_plate_filepath)
-bpy.ops.wm.addon_enable(module='holder')
+tu_plugin_filepath = os.path.join(current_dir, 'tactile_universe_plugin.zip')
+bpy.ops.preferences.addon_install(filepath=tu_plugin_filepath)
+bpy.ops.preferences.addon_enable(module='tactile_universe_plugin')
 
 # enable import images as plane
-bpy.ops.wm.addon_enable(module='io_import_images_as_planes')
+bpy.ops.preferences.addon_enable(module='io_import_images_as_planes')
 
 # save user preferences
 bpy.ops.wm.save_userpref()
